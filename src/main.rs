29694a347b5e0d8fbeb13fe8de42c40c27c18b10
maancham <houmaan@interoplabs.io>
2025-06-19 10:19:40 -0400

    respond with empty 200

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,23 @@
 #[allow(unused_imports)]
-use std::net::TcpListener;
 
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
 
-    // Uncomment this block to pass the first stage
-    
+fn main() {    
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     
-    for stream in listener.incoming() {
-        match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+    for stream_result in listener.incoming() {
+        let stream= stream_result.unwrap();
+        println!("Connection established!");
+        handle_client(stream);
     }
+}
+
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buf = [0; 512];
+    let _read_bufsize = stream.read(&mut buf).expect("failed to read from client");
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    stream.write_all(response.as_bytes()).expect("failed to write to client");    
 }
