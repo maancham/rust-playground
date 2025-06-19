b399f61cb9602f97c243318f14a09453c1a09ccc
maancham <houmaan@interoplabs.io>
2025-06-19 11:26:37 -0400

    extract url path

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,6 @@ fn main() {
     
     for stream_result in listener.incoming() {
         let stream= stream_result.unwrap();
-        println!("Connection established!");
         handle_client(stream);
     }
 }
@@ -16,8 +15,16 @@ fn main() {
 
 fn handle_client(mut stream: TcpStream) {
     let mut buf = [0; 512];
-    let _read_bufsize = stream.read(&mut buf).expect("failed to read from client");
+    let read_bufsize = stream.read(&mut buf).expect("failed to read from client");
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let request = String::from_utf8_lossy(&buf[0..read_bufsize]);
+    let first_line = request.lines().next().unwrap();
+    let parts: Vec<&str> = first_line.split_whitespace().collect();
+
+    let response = match parts[1] {
+        "/" => "HTTP/1.1 200 OK\r\n\r\n",
+        _ => "HTTP/1.1 404 Not Found\r\n\r\n",
+    };
+
     stream.write_all(response.as_bytes()).expect("failed to write to client");    
 }
