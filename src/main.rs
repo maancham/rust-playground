8e8bd03f9a5c23f404e1fa5a9694077b6e6b2153
maancham <houmaan@interoplabs.io>
2025-06-26 11:15:03 -0400

    user-agent

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,32 @@
-#[allow(unused_imports)]
-
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
 
-fn main() {    
+fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-    
+
     for stream_result in listener.incoming() {
-        let stream= stream_result.unwrap();
-        handle_client(stream);
+        let stream = stream_result.unwrap();
+        handle_connection(stream);
     }
 }
 
-
-fn handle_client(mut stream: TcpStream) {
-    let mut buf = [0; 512];
+fn handle_connection(mut stream: TcpStream) {
+    let mut buf = [0; 1024];
     let read_bufsize = stream.read(&mut buf).expect("failed to read from client");
 
     let request = String::from_utf8_lossy(&buf[0..read_bufsize]);
-    let first_line = request.lines().next().unwrap();
+    let lines: Vec<&str> = request.lines().collect();
+
+    let first_line = lines.first().unwrap();
     let parts: Vec<&str> = first_line.split_whitespace().collect();
 
-    let response = match parts[1] {
+    if parts.len() < 2 {
+        return;
+    }
+
+    let path = parts[1];
+
+    let response = match path {
         "/" => "HTTP/1.1 200 OK\r\n\r\n",
         path if path.starts_with("/echo/") => {
             if let Some(post_path) = path.strip_prefix("/echo/") {
@@ -30,13 +35,25 @@ fn handle_client(mut stream: TcpStream) {
                     post_path.len(),
                     post_path
                 )
+            } else {
+                "HTTP/1.1 404 Not Found\r\n\r\n"
             }
-            else {
-               "HTTP/1.1 404 Not Found\r\n\r\n" 
-            }
-        },
+        }
+        "/user-agent" => {
+            let agent = lines.iter()
+                .find(|line| line.starts_with("User-Agent"))
+                .map(|line| line.split(":").nth(1).unwrap().trim()).unwrap();
+            
+            &format!(
+                "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+                agent.len(),
+                agent
+            )
+        }
         _ => "HTTP/1.1 404 Not Found\r\n\r\n",
     };
 
-    stream.write_all(response.as_bytes()).expect("failed to write to client");    
+    stream
+        .write_all(response.as_bytes())
+        .expect("failed to write to client");
 }
