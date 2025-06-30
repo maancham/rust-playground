b270626e77cf044a525a7a48ffbd0cab0c2478e3
maancham <houmaan@interoplabs.io>
2025-06-30 10:01:23 -0400

    post files

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,13 @@ use clap::Parser;
 
 #[derive(Parser, Debug)]
 struct Args {
-    #[arg(long, default_value = "/tmp/")]
+    #[arg(long, default_value = "./tmp")]
     directory: String,
 }
 
 #[tokio::main]
 async fn main() {
     let args = Args::parse();
-    println!("Using directory: {}", args.directory);
 
     let listener = TcpListener::bind("127.0.0.1:4221").await.unwrap();
 
@@ -29,59 +28,105 @@ async fn handle_connection(mut stream: TcpStream, file_directory: String) {
     let mut buf = [0; 1024];
     let read_bufsize = stream.read(&mut buf).await.unwrap();
 
-    let request = String::from_utf8_lossy(&buf[0..read_bufsize]);
-    let lines: Vec<&str> = request.lines().collect();
+    let mut headers = [httparse::EMPTY_HEADER; 64];
+    let mut req = httparse::Request::new(&mut headers);
+    let request_result = req.parse(&buf[0..read_bufsize]);
 
-    let first_line = lines.first().unwrap();
-    let parts: Vec<&str> = first_line.split_whitespace().collect();
+    match request_result {
+        Ok(httparse::Status::Complete(headers_end)) => {
+            let method = req.method.unwrap();
+            let path = req.path.unwrap();
 
-    if parts.len() < 2 {
-        return;
+            let content_length = find_header(&req.headers, "content-length");
+            let user_agent = find_header(&req.headers, "user-agent");
+
+
+            let response = match (method, path) {
+                ("GET", "/") => &format_response(200, "", ""),
+                ("GET", "/user-agent") => {
+                    if let Some(agent) = user_agent {
+                        &format_response(200, "text/plain", agent)
+                    }
+                    else {
+                        &format_response(404, "", "")
+                    }
+                }
+                ("GET", path) if path.starts_with("/echo/") => {
+                    if let Some(post_path) = path.strip_prefix("/echo/") {
+                        &format_response(200, "text/plain", post_path)
+                    } else {
+                        &format_response(404, "", "")
+                    }
+                }
+                ("GET", path) if path.starts_with("/files/") => {
+                    let file_path = format!("{}/{}", file_directory, path.strip_prefix("/files/").unwrap());
+                    if let Ok(file_content) = fs::read_to_string(file_path) {
+                        &format_response(200, "application/octet-stream", &file_content)
+                    } else {
+                        &format_response(404, "", "")
+                    }
+                }
+                ("POST", path) if path.starts_with("/files/") => {
+                    let file_path = format!("{}/{}", file_directory, path.strip_prefix("/files/").unwrap());
+
+                    let mut body = Vec::new();
+                    body.extend_from_slice(&buf[headers_end..read_bufsize]);
+
+                    if let Some(content_len_str) = content_length {
+                        if let Ok(content_len) = content_len_str.parse::<usize>() {
+                            let remaining_bytes = content_len.saturating_sub(body.len());
+                            if remaining_bytes > 0 {
+                                let mut remaining_buf = vec![0; remaining_bytes];
+                                stream.read_exact(&mut remaining_buf).await.unwrap();
+                                body.extend_from_slice(&remaining_buf);
+                            }
+                        }
+                    }
+
+                    fs::write(file_path, body).unwrap();
+                    &format_response(201, "", "")
+                }
+                _ =>  &format_response(404, "", ""),
+            };
+
+            stream
+                .write_all(response.as_bytes())
+                .await.unwrap();
+
+        }
+        Ok(httparse::Status::Partial) => {
+            println!("received partial request");
+        },
+        Err(e) => {
+            println!("failed to parse request: {:?}", e);
+        }
     }
+}
 
-    let path = parts[1];
+fn format_response(status: u16, content_type: &str, body: &str) -> String {
+    format!(
+        "HTTP/1.1 {} {}\r\nContent-Type: {}\r\nContent-Length: {}\r\n\r\n{}",
+        status,
+        status_text(status),
+        content_type,
+        body.len(),
+        body
+    )
+}
 
-    let response = match path {
-        "/" => "HTTP/1.1 200 OK\r\n\r\n",
-        path if path.starts_with("/echo/") => {
-            if let Some(post_path) = path.strip_prefix("/echo/") {
-                &format!(
-                    "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
-                    post_path.len(),
-                    post_path
-                )
-            } else {
-                "HTTP/1.1 404 Not Found\r\n\r\n"
-            }
-        }
-        "/user-agent" => {
-            let agent = lines.iter()
-                .find(|line| line.starts_with("User-Agent"))
-                .map(|line| line.split(":").nth(1).unwrap().trim()).unwrap();
-            
-            &format!(
-                "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
-                agent.len(),
-                agent
-            )
-        }
-        path if path.starts_with("/files/") => {
-            let file_path = format!("{}/{}", file_directory, path.strip_prefix("/files/").unwrap());
+fn status_text(status: u16) -> &'static str {
+    match status {
+        200 => "OK",
+        201 => "Created",
+        404 => "Not Found",
+        400 => "Bad Request",
+        _ => "Unknown",
+    }
+}
 
-            if let Ok(file_content) = fs::read_to_string(file_path) {
-                &format!(
-                    "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {}\r\n\r\n{}",
-                    file_content.len(),
-                    file_content
-                )
-            } else {
-                "HTTP/1.1 404 Not Found\r\n\r\n"
-            }
-        }
-        _ => "HTTP/1.1 404 Not Found\r\n\r\n",
-    };
-
-    stream
-        .write_all(response.as_bytes())
-        .await.unwrap();
+fn find_header<'a>(headers: &'a [httparse::Header], name: &str) -> Option<&'a str> {
+    headers
+        .iter()
+        .find(|head| head.name.to_lowercase() == name.to_lowercase())
+        .and_then(|head| std::str::from_utf8(head.value).ok())
 }
